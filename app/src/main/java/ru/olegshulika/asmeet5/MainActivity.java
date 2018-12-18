689b0926155dee8e7119e0c83c4b784ff8fed8df
OlegShulika <olegshulika@gmail.com>
2018-12-18 15:36:29 +0300

    time service, cell 2, cell 4

--- app/src/main/java/ru/olegshulika/asmeet5/MainActivity.java
+++ app/src/main/java/ru/olegshulika/asmeet5/MainActivity.java
@@ -1,11 +1,26 @@
 package ru.olegshulika.asmeet5;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.solver.widgets.ConstraintHorizontalLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainAct";
@@ -17,11 +32,66 @@ public class MainActivity extends AppCompatActivity {
     private TextView mC2Text3;
     private TextView mC2Text4;
     private TextView mC2Text5;
-    private TextView mC3Text1;  // Cell 1 (2,1) LinearLayout - vertical
+    private TextView mC3Text1;  // Cell 3 (2,1) LinearLayout - vertical
     private TextView mC3Text2;
     private TextView mC3Text3;
-    private Button mC4Button1;  // Cell 1 (2,2) ConstantLayout  - circle
+    private Button mC4Button1;  // Cell 4 (2,2) ConstantLayout  - circle
     private Button mC4Button2;
+    private boolean mC4running=true;
+    private int mC4direction=1;     // +1 (CW)  -1(CCW)
+    private long mc4delta=0;
+
+    private Messenger mTimeCell24Messenger = new Messenger(new IncomingTimeHandler());
+    private Messenger mTimeServiceMessenger;
+
+    private ServiceConnection mServiceConnection = new ServiceConnection()  {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mTimeServiceMessenger = new Messenger(service);
+            Message msg = Message.obtain(null, MsgCommand.REGISTER_CLIENT);
+            msg.replyTo = mTimeCell24Messenger;
+            try {
+                mTimeServiceMessenger.send(msg);
+            } catch(RemoteException ex) {
+                ex.printStackTrace();
+            }
+            Log.d(TAG, "TimeService connected");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mTimeServiceMessenger=null;
+            Log.d(TAG, "TimeService disconnected");
+        }
+    };
+
+    private class IncomingTimeHandler extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            Long msec = (Long)msg.obj;
+            mC2Text1.setText(getDateTime(msec,"dd/MM/yyyy"));
+            mC2Text2.setText(getDateTime(msec,"hh:mm"));
+            mC2Text3.setText(getDateTime(msec,".SSS"));
+            mC2Text4.setText(getDateTime(msec,"ss"));
+            mC2Text5.setText("ms="+msec);
+
+            if (mC4running) {
+                ConstraintLayout.LayoutParams lp = (ConstraintLayout.LayoutParams) mC4Button1.getLayoutParams();
+                long degree = (((msec / 1000) % 60) * 6 * mC4direction + 360 + mc4delta) % 360;
+                lp.circleAngle = (float) degree;
+                mC4Button1.setText(String.valueOf(degree) + (char) 0x00B0);
+                mC4Button1.setLayoutParams(lp);
+            }
+        }
+    }
+
+    private static String getDateTime(long milliSeconds, String dateFormat)
+    {
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,34 +124,56 @@ public class MainActivity extends AppCompatActivity {
         mC4Button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mC4direction *= -1;
             }
         });
 
         mC4Button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mC4running ^= true;
+                mC4Button2.setText(mC4running? "STOP":"RUN");
             }
         });
     }
 
+    public void bindTimeService() {
+        Log.d(TAG, "binding TimeService...");
+        bindService(TimeService.newIntent(MainActivity.this),
+                    mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    public void unbindTimeService() {
+        Log.d(TAG, "unbinding TimeService...");
+        Message msg = Message.obtain(null, MsgCommand.UNREGISTER_CLIENT);
+        msg.replyTo = mTimeCell24Messenger;
+        try {
+            mTimeServiceMessenger.send(msg);
+        } catch(RemoteException ex) {
+            ex.printStackTrace();
+        }
+        unbindService(mServiceConnection);
+    }
+
+
     @Override
     protected void onStart() {
-        super.onStart();
         Log.d(TAG, " onStart");
+        super.onStart();
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
         Log.d(TAG, " onResume");
+        super.onResume();
+        bindTimeService();
     }
 
     @Override
     protected void onPause() {
-        super.onPause();
         Log.d(TAG, " onPause");
+        super.onPause();
+        unbindTimeService();
     }
 
     @Override
--- app/src/main/java/ru/olegshulika/asmeet5/TimeService.java
+++ app/src/main/java/ru/olegshulika/asmeet5/TimeService.java
@@ -3,129 +3,123 @@ package ru.olegshulika.asmeet5;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
 
-@interface Command {
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+@interface MsgCommand {
     int INVALID = -1;
-    int STOP = 0;
-    int START = 1;
+    int REGISTER_CLIENT = 0;
+    int UNREGISTER_CLIENT = 1;
+    int SET_VALUE = 2;
 }
 
 public class TimeService extends Service {
-    private static final int MODE = Service.START_NOT_STICKY;
     private static final String TAG = "_TimeService";
-    private static final String KEY_COMMAND = "timeservice.cmd";
-    private static final String KEY_DATA = "timeservice.data";
-    private static final int SERVICE_WORKTIME_LIMIT_MS = 1000000;   // max work time in msec
-    private static final int SERVICE_TIMESTEP_MS = 100;             // service 1 step time in msec
-    private IBinder mBinder = new LocalBinder();
-    private Handler handlerMsg = null;
-    private boolean serviceStarted = false;
+    private List<Messenger> mClients = new ArrayList<Messenger>();
+    private Messenger mMessenger = new Messenger (new IncomingHandler());
+    private static final int SERVICE_WORKTIME_LIMIT = 1000000;   // max work time in msec
+    private static final int SERVICE_WORKTIME_STEP = 100;   // work step in msec
 
+    private class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MsgCommand.REGISTER_CLIENT:
+                    mClients.add(msg.replyTo);
+                    if (!isServiceStarted()) {
+                        startSrv();
+                        setServiceStarted(true);
+                    }
+                    break;
+                case MsgCommand.UNREGISTER_CLIENT:
+                    mClients.remove(msg.replyTo);
+                    if (mClients.isEmpty()) {
+                        stopSelf();
+                        setServiceStarted(false);
+                    }
+                    break;
+                case MsgCommand.SET_VALUE:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    private boolean serviceStarted = false;
     private boolean isServiceStarted() {
         return serviceStarted;
     }
-
     private void setServiceStarted(boolean serviceStarted) {
         this.serviceStarted = serviceStarted;
-    }
-
-    public class LocalBinder extends Binder {
-        TimeService getService() {
-            return TimeService.this;
-        }
-        void setHandler(Handler handlerM) {
-            handlerMsg = handlerM;
-        }
-    }
-
-    private TimeService() {
-    }
-    @Override
-
-    public void onCreate() {
-        Log.d(TAG, " onCreate");
-        super.onCreate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, final int startId) {
-        Log.d(TAG, " onStartCommand "+flags+" "+startId);
-        int cmd = getCommand(intent);
-        if (cmd==Command.INVALID)
-            return MODE;
-        // start service
-        if (cmd==Command.START && !isServiceStarted()) {
-            Log.d(TAG, " started..." + startId);
-            setServiceStarted(true);
-            startSrv(startId);
-            return MODE;
-        }
-        // stop service
-        if (cmd==Command.STOP && isServiceStarted()) {
-            Log.d(TAG, " stopped...");
-            stopSelf(startId);
-            setServiceStarted(false);
-            return MODE;
-        }
-
-        return MODE;
-    }
-
-    private void startSrv(final int startId) {              // Service workout thread
-        new Thread(new Runnable() {         // Start service
-            @Override
-            public void run() {
-                int workTime=SERVICE_WORKTIME_LIMIT_MS;
-                while (workTime>0 && isServiceStarted()){
-                    workTime -= SERVICE_TIMESTEP_MS;
-                    try {
-                        Thread.sleep(SERVICE_TIMESTEP_MS);             // wait 1 sec
-                        String strMsg = "msg"+System.currentTimeMillis();
-                        Log.d(TAG,"==>"+strMsg);
-                        if (handlerMsg!=null) {
-                            Message msg = handlerMsg.obtainMessage(0, strMsg);
-                            handlerMsg.sendMessage(msg);
-                        }
-                    } catch (Exception ex){
-                        Log.d(TAG,ex.getMessage());
-                    }
-                }
-                stopSelf(startId);
-                setServiceStarted(false);
-            }
-        }).start();
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    public static final Intent newIntent(Context context, int serviceCommand){
-        Intent intent = new Intent(context, TimeService.class);
-        intent.putExtra(KEY_COMMAND, serviceCommand);
-        return intent;
     }
 
     public static final Intent newIntent(Context context){
         return new Intent(context, TimeService.class);
     }
 
-    public int getCommand (Intent intent) {
-        return intent.getIntExtra(KEY_COMMAND, Command.INVALID);
+    private void startSrv() {              // Service workout thread
+        new Thread(new Runnable() {         // Start service
+            @Override
+            public void run() {
+                Log.d(TAG,"StartSrv()");
+                int workTime=SERVICE_WORKTIME_LIMIT;
+                while (workTime>0 && isServiceStarted()){
+                    workTime-=SERVICE_WORKTIME_STEP;
+                    try {
+                        Thread.sleep(SERVICE_WORKTIME_STEP);             // wait some time
+                        long currentTimeMs = System.currentTimeMillis();
+                        //Log.d(TAG, "==>"+currentTimeMs);
+                        for(Messenger clientMsgr : mClients){
+                            Message msg = Message.obtain(null,0, (Long)currentTimeMs);
+                            clientMsgr.send(msg);
+                        }
+                    }
+                    catch (Exception ex){
+                        Log.d(TAG,ex.getMessage());
+                    }
+                }
+                stopSelf();
+                setServiceStarted(false);
+            }
+        }).start();
+    }
+
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, " onCreate");
+        super.onCreate();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d(TAG, " onBind");
+        return mMessenger.getBinder();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.d(TAG, " onUnbind");
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.d(TAG, " onRebind");
+        super.onRebind(intent);
     }
 
     @Override
     public void onDestroy() {
         Log.d(TAG, " onDestroy");
         stopSelf();
-        setServiceStarted(false);
         super.onDestroy();
     }
 
